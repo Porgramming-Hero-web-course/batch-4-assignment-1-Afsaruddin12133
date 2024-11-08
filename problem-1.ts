1a517a9b54e7f85ddebfacf2c3bcdf5a617befed
Afsaruddin12133 <afsaruddin12133@gmail.com>
2024-11-08 17:18:56 +0600

    Complete Problem - 1_

--- problem-1.ts
+++ problem-1.ts
@@ -2,12 +2,12 @@
     //
     function sumArray (array : number[]) : number{
 
-        let sum = 0;
+        let sum : number = 0;
         array.map((num : number) => sum += num);
         return sum;
     }
 
-    const result = sumArray([1,2,3,4,5]);
+    const result : number = sumArray([1,2,3,4,5]);
     console.log(result);
     
     //
