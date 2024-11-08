3e7c5487dd85a0421c88cf2cac1f693c9badf6b0
Afsaruddin12133 <afsaruddin12133@gmail.com>
2024-11-08 17:16:28 +0600

    Complete Problem - 1

--- problem-1.ts
+++ problem-1.ts
@@ -1,1 +1,14 @@
-console.log("First Commmit");
+{
+    //
+    function sumArray (array : number[]) : number{
+
+        let sum = 0;
+        array.map((num : number) => sum += num);
+        return sum;
+    }
+
+    const result = sumArray([1,2,3,4,5]);
+    console.log(result);
+    
+    //
+}
